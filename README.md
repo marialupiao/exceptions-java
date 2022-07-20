f31cdd30f1fe2dd51e7794bf100f2ee42770ced6
Maria <marialupiao@outlook.com>
2022-07-19 22:28:50 -0300

    updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# exceptions-java
+# Tratamento de Exceções em Java
+
+Exercícios propostos pelo curso de Tratamento de Exceções em Java da plataforma Dio.me
+Parte do Bootcamp Santander Code Girls :heart_decoration:
--- src/exceptions/DivisaoNaoExataException.java
+++ src/exceptions/DivisaoNaoExataException.java
@@ -1,2 +1,6 @@
-package exceptions;public class DivisaoNaoExataException {
+package exceptions;
+
+public class DivisaoNaoExataException extends Throwable {
+    public DivisaoNaoExataException(String s, int i, int i1) {
+    }
 }
